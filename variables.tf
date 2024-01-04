8fac63620f66c8cf8079b115db12c37474f74d14
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2024-01-04 22:26:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,50 +4,74 @@ variable "name" {
   default     = "testmod"
 }
 
-variable "attributes" {
-  type        = list(map(string))
-  default     = []
-}
 
 variable "hash_key" {
-  type        = string
-  default     = "UserId"
+  type    = string
+  default = "TestTableHashKey"
+}
+
+variable "hash_key_type" {
+  type    = string
+  default = "S"
+}
+
+variable "range_key_type" {
+  type    = string
+  default = "S"
 }
 
 variable "range_key" {
-  type        = string
-  default     = "GameTitle"
+  type    = string
+  default = "TestTableRangeKey"
+}
+
+variable "attributes" {
+  default = [
+    {
+      name = "TestTableHashKey",
+      type = "S",
+    },
+    {
+      name = "TestTableRangeKey",
+      type = "S",
+    },
+  ]
+  type = list(object({ name = string, type = string }))
+}
+
+variable "global_secondary_index_map" {
+  type = list(object({
+    hash_key           = string
+    name               = string
+    non_key_attributes = list(string)
+    projection_type    = string
+    range_key          = string
+    read_capacity      = number
+    write_capacity     = number
+  }))
+  default     = []
+  description = "Additional global secondary indexes in the form of a list of mapped values"
 }
 
 variable "billing_mode" {
-  type        = string
-  default     = "PAY_PER_REQUEST"
+  type    = string
+  default = "PROVISIONED"
 }
 
 variable "write_capacity" {
-  type        = number
-  default     = "5"
+  type    = number
+  default = "5"
 }
 
 variable "read_capacity" {
-  type        = number
-  default     = "5"
+  type    = number
+  default = "5"
 }
 
-variable "ttl_enabled" {
-  description = "Indicates whether ttl is enabled"
-  type        = bool
-  default     = false
-}
-
-variable "ttl_attribute_name" {
-  description = "The name of the table attribute to store the TTL timestamp in"
-  type        = string
-  default     = "TimeToExist"
-}
 
 variable "global_secondary_indexes" {
   description = "Describe a GSI for the table; subject to the normal limits on the number of GSIs, projected attributes, etc."
   type        = any
   default     = []
 }
+
