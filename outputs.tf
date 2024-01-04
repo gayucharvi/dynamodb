00fc8b604e5465a75fd75c1932e2da735e256240
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2024-01-04 22:31:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,5 @@
-output "table_name" {
-  value       = join("", aws_dynamodb_table.default[*].name)
-  description = "DynamoDB table name"
-}
+output "dynamodb" {
+  value       = aws_dynamodb_table.dynamodb
+  }
 
-output "table_id" {
-  value       = join("", aws_dynamodb_table.default[*].id)
-  description = "DynamoDB table ID"
-}
 
-output "table_arn" {
-  value       = join("", aws_dynamodb_table.default[*].arn)
-  description = "DynamoDB table ARN"
-}
-
-output "global_secondary_index_names" {
-  value       = null_resource.global_secondary_index_names[*].triggers.name
-  description = "DynamoDB secondary index names"
-}
